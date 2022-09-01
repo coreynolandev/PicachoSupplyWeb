b28185a565e1f7caf755e1406925d57b9e897654
coreynolan <coreynolan24dev@gmail.com>
2022-09-01 02:16:18 -0400

    init edits

--- src/App.js
+++ src/App.js
@@ -1,45 +1,114 @@
 import logo from './logo.svg';
 import './App.css';
-import BlackPink from './assets/BlackPink';
-import BlueDarkBlueGradient from './assets/BlueDarkBlueGradient';
-import GreenBlueGradient from './assets/GreenBlueGradient';
-import LightBlue from './assets/LightBlue';
-import PinkGradient from './assets/PinkGradient';
-import PinkOrangeGradient from './assets/PinkOrangeGradient';
-import PinkYellowGradient from './assets/PinkYellowGradient';
-import WhiteBlack from './assets/WhiteBlack';
-import YellowBlack from './assets/YellowBlack';
-import mainLogo from './assets/new.png';
+import LogoBlue from './assets/toxic-wave-logo/logo_blue.png';
+import LogoBlueGreenGradient from './assets/toxic-wave-logo/logo_blue_green_gradient.png';
+import LogoPinkOrangeGradient from './assets/toxic-wave-logo/logo_pink_orange_gradient.png';
+import LogoWhite from './assets/toxic-wave-logo/logo_white.png';
+import StitchSwatchBlue from './assets/swatch/swatch_blue.png';
+import StitchSwatchBlueGreenGradient from './assets/swatch/swatch_blue_green_gradient.png';
+import StitchSwatchPinkOrangeGradient from './assets/swatch/swatch_pink_orange_gradient.png';
+import StitchSwatchWhite from './assets/swatch/swatch_white.png';
+import HoodieArmy from './assets/hoodie/hoodie_army.png';
+import HoodieBlack from './assets/hoodie/hoodie_black.png';
+import HoodieBlue from './assets/hoodie/hoodie_blue.png';
+import HoodieLavender from './assets/hoodie/hoodie_lavender.png';
+import HoodiePlum from './assets/hoodie/hoodie_plum.png';
+import HoodieYellow from './assets/hoodie/hoodie_yellow.png';
+import { forwardRef, useState } from 'react';
+import Swatch from './components/Swatch';
+import { Container, IconButton, StepButton, Tooltip, Typography } from '@mui/material';
 
-import { ReactComponent as NewOne } from './assets/ne2w.svg';
+import HoodieSwatchArmy from './assets/hoodie/swatch/army.png';
+import HoodieSwatchBlack from './assets/hoodie/swatch/black.png';
+import HoodieSwatchBlue from './assets/hoodie/swatch/blue.png';
+import HoodieSwatchLavender from './assets/hoodie/swatch/lavender.png';
+import HoodieSwatchPlum from './assets/hoodie/swatch/plum.png';
+import HoodieSwatchYellow from './assets/hoodie/swatch/yellow.png';
+
+// import { Swatch } from '@sajari/react-components';
 
 function App() {
+	const [selectedSwatch, setSelectedSwatch] = useState(0);
+	const [selectedHoodie, setSelectedHoodie] = useState(0);
+
+	const swatchSelectionList = [
+		{ logo: LogoBlue, swatchImage: StitchSwatchBlue, alt: 'Blue', order: 0 },
+		{ logo: LogoBlueGreenGradient, swatchImage: StitchSwatchBlueGreenGradient, alt: 'Blue Green Gradient', order: 1 },
+		{ logo: LogoPinkOrangeGradient, swatchImage: StitchSwatchPinkOrangeGradient, alt: 'Pink Orange Gradient', order: 2 },
+		{ logo: LogoWhite, swatchImage: StitchSwatchWhite, alt: 'White', order: 3 }
+	];
+
+	const hoodieSelectionList = [
+		{ logo: HoodieArmy, swatchImage: HoodieSwatchArmy, alt: 'Army', order: 0 },
+		{ logo: HoodieBlack, swatchImage: HoodieSwatchBlack, alt: 'Black', order: 1 },
+		{ logo: HoodieBlue, swatchImage: HoodieSwatchBlue, alt: 'Blue', order: 2 },
+		{ logo: HoodieLavender, swatchImage: HoodieSwatchLavender, alt: 'Lavender', order: 3 },
+		{ logo: HoodiePlum, swatchImage: HoodieSwatchPlum, alt: 'Plum', order: 4 },
+		{ logo: HoodieYellow, swatchImage: HoodieSwatchYellow, alt: 'Yellow', order: 5 }
+	];
+
 	return (
 		<div className='App'>
-			<header className='App-header'>
-				<img src={logo} className='App-logo' alt='logo' />
-				<p>
-					Edit <code>src/App.js</code> and save to reload.
-				</p>
-				<a className='App-link' href='https://reactjs.org' target='_blank' rel='noopener noreferrer'>
-					{/* <span dangerouslySetInnerHTML={{ __html: '<svg>...</svg>' }} />; Learn React */}
-				</a>
-
-				<BlackPink />
-				<BlueDarkBlueGradient />
-				<GreenBlueGradient />
-				<LightBlue />
-				<PinkGradient />
-				<PinkOrangeGradient />
-				<PinkYellowGradient />
-				<WhiteBlack />
-				<YellowBlack />
-				<NewOne />
-				<div className='container'>
-					<img src={mainLogo} alt='fire' />
+			<Container maxWidth='sm'>
+				<div className='hoodie-stitch container'>
+					<img className='toxic-wave-logo hoodie-stitch stitch' src={swatchSelectionList[selectedSwatch].logo} alt='Stitch' />
+					<img className='hoodie-base hoodie-stitch hoodie' src={hoodieSelectionList[selectedHoodie].logo} alt='Hoodie' />
 				</div>
-				<img src='./assets/new.png' alt='corey'></img>
-			</header>
+				<div className='swatch-group'>
+					<div className='swatch-selector-group'>
+						<Typography sx={{ width: 100 }}>Stitching</Typography>
+
+						{swatchSelectionList.map((swatch) => {
+							return (
+								<Tooltip title={swatch.alt} placement='top'>
+									<IconButton
+										sx={{
+											borderRadius: 0,
+											borderColor: 'primary.main',
+											margin: 0,
+											padding: 0
+										}}>
+										<Swatch
+											swatchImage={swatch.swatchImage}
+											type='color-swatch'
+											alt={swatch.alt}
+											number={swatch.order}
+											selectedSwatch={selectedSwatch}
+											setSelectedSwatch={setSelectedSwatch}
+										/>
+									</IconButton>
+								</Tooltip>
+							);
+						})}
+					</div>
+
+					<div className='swatch-selector-group'>
+						<Typography sx={{ width: 100 }}>Base</Typography>
+						{hoodieSelectionList.map((swatch) => {
+							return (
+								<Tooltip title={swatch.alt}>
+									<IconButton
+										sx={{
+											borderRadius: 0,
+											borderColor: 'primary.main',
+											margin: 0,
+											padding: 0
+										}}>
+										<Swatch
+											swatchImage={swatch.swatchImage}
+											type='color-swatch'
+											alt={swatch.alt}
+											number={swatch.order}
+											selectedSwatch={selectedHoodie}
+											setSelectedSwatch={setSelectedHoodie}
+										/>
+									</IconButton>
+								</Tooltip>
+							);
+						})}
+					</div>
+				</div>
+			</Container>
 		</div>
 	);
 }
---
+++ src/components/Swatch.js
@@ -0,0 +1,12 @@
+const Swatch = ({ swatchImage, type, alt, number, selectedSwatch, setSelectedSwatch }) => {
+	const isSelected = number === selectedSwatch;
+	const labelName = `${alt}${number}swatch`;
+	return (
+		<label htmlFor={labelName} onClick={() => setSelectedSwatch(number)}>
+		{/* <label htmlFor={labelName} title={alt} onClick={() => setSelectedSwatch(number)}> */}
+			<img id={labelName} src={swatchImage} className={`${type}${isSelected ? ' selected' : ''}`} alt={alt} />
+		</label>
+	);
+};
+
+export default Swatch;
