29c176eb60fde6430a33aad6a485ca4bbaf88ddc
coreynolan <coreynolan24dev@gmail.com>
2023-02-02 19:11:26 -0500

    update css and hover

--- src/pages/Shop.js
+++ src/pages/Shop.js
@@ -14,16 +14,18 @@ const Shop = () => {
 	const [krownHover, setKrownHover] = useState(false);
 
 	return (
-		<Grid container spacing={3} mb={2} p={2} justifyContent='center'>
+		<Grid container spacing={3} mb={2} p={3} justifyContent='center'>
 			<Grid item sm={12} md={6} className=' shop-card'>
 				<Card raised>
-					<Box className='shop-card-content' component='a' href='/hoodies'>
+					<Box component='a' href='/hoodies' sx={{ display: 'flex', height: '100%' }}>
 						<img
 							src={hoodieHover ? HoodieThumbnailHover : HoodieThumbnail}
 							onMouseOver={() => setHoodieHover(true)}
 							onMouseOut={() => setHoodieHover(false)}
+							onTouchStart={() => setHoodieHover(true)}
+							onTouchEnd={() => setHoodieHover(false)}
 							alt='Lifestyle Hoodie'
-							className='shop-card-content thumbnail'
+							className='shop-card-content'
 						/>
 					</Box>
 				</Card>
@@ -31,13 +33,15 @@ const Shop = () => {
 
 			<Grid item sm={12} md={6} className=' shop-card'>
 				<Card raised>
-					<Box className='shop-card-content' component='a' href='/explorer-hat'>
+					<Box component='a' href='/explorer-hat' sx={{ display: 'flex', height: '100%' }}>
 						<img
 							src={explorerHover ? ExplorerThumbnailHover : ExplorerThumbnail}
 							onMouseOver={() => setExplorerHover(true)}
 							onMouseOut={() => setExplorerHover(false)}
+							onTouchStart={() => setExplorerHover(true)}
+							onTouchEnd={() => setExplorerHover(false)}
 							alt='Explorer Hat'
-							className='shop-card-content thumbnail'
+							className='shop-card-content '
 						/>
 					</Box>
 				</Card>
@@ -45,13 +49,15 @@ const Shop = () => {
 
 			<Grid item sm={12} md={6} className=' shop-card'>
 				<Card raised>
-					<Box className='shop-card-content link' sx={{ cursor: 'auto  !important' }}>
+					<Box sx={{ display: 'flex', height: '100%' }}>
 						<img
 							src={krownHover ? KrownThumbnailHover : KrownThumbnail}
 							onMouseOver={() => setKrownHover(true)}
 							onMouseOut={() => setKrownHover(false)}
+							onTouchStart={() => setKrownHover(true)}
+							onTouchEnd={() => setKrownHover(false)}
 							alt='Krown Hat'
-							className='shop-card-content thumbnail'
+							className='shop-card-content'
 						/>
 					</Box>
 				</Card>
