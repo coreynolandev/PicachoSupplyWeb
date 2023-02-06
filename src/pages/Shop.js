a69d0c78617822395859e269d15919103f77ee85
coreynolan <coreynolan24dev@gmail.com>
2023-02-05 21:29:57 -0500

    add labels to shop

--- src/pages/Shop.js
+++ src/pages/Shop.js
@@ -1,4 +1,4 @@
-import { Box, Card, Grid } from '@mui/material';
+import { Box, Button, Card, Grid, Stack, Typography } from '@mui/material';
 
 import KrownThumbnail from '../assets2/shop/KROWN Icon.png';
 import KrownThumbnailHover from '../assets2/shop/KROWN Icon Hover.png';
@@ -15,52 +15,79 @@ const Shop = () => {
 
 	return (
 		<Grid container spacing={3} mb={2} p={3} justifyContent='center'>
-			<Grid item sm={12} md={6} className=' shop-card'>
-				<Card raised>
-					<Box component='a' href='/hoodies' sx={{ display: 'flex', height: '100%' }}>
-						<img
-							src={hoodieHover ? HoodieThumbnailHover : HoodieThumbnail}
-							onMouseOver={() => setHoodieHover(true)}
-							onMouseOut={() => setHoodieHover(false)}
-							onTouchStart={() => setHoodieHover(true)}
-							onTouchEnd={() => setHoodieHover(false)}
-							alt='Lifestyle Hoodie'
-							className='shop-card-content'
-						/>
-					</Box>
-				</Card>
+			<Grid
+				item
+				sm={12}
+				md={4}
+				className=' shop-card-content'
+				onMouseOver={() => setHoodieHover(true)}
+				onMouseOut={() => setHoodieHover(false)}
+				onTouchStart={() => setHoodieHover(true)}
+				onTouchEnd={() => setHoodieHover(false)}>
+				<Stack direction='column' spacing={1} component='a' href='/hoodies' sx={{ textDecoration: 'none' }}>
+					<Card raised>
+						<Box sx={{ display: 'flex', height: '100%' }}>
+							<img src={hoodieHover ? HoodieThumbnailHover : HoodieThumbnail} alt='Lifestyle Hoodie' className='shop-card-content' />
+						</Box>
+					</Card>
+
+					<Card raised>
+						<Stack direction={'row'} justifyContent='space-between' p={1}>
+							<Typography>Lifestyle Hoodie</Typography>
+							<Typography>$50.00</Typography>
+						</Stack>
+					</Card>
+				</Stack>
 			</Grid>
 
-			<Grid item sm={12} md={6} className=' shop-card'>
-				<Card raised>
-					<Box component='a' href='/explorer-hat' sx={{ display: 'flex', height: '100%' }}>
-						<img
-							src={explorerHover ? ExplorerThumbnailHover : ExplorerThumbnail}
-							onMouseOver={() => setExplorerHover(true)}
-							onMouseOut={() => setExplorerHover(false)}
-							onTouchStart={() => setExplorerHover(true)}
-							onTouchEnd={() => setExplorerHover(false)}
-							alt='Explorer Hat'
-							className='shop-card-content '
-						/>
-					</Box>
-				</Card>
+			<Grid
+				item
+				sm={12}
+				md={4}
+				className=' shop-card-content'
+				onMouseOver={() => setExplorerHover(true)}
+				onMouseOut={() => setExplorerHover(false)}
+				onTouchStart={() => setExplorerHover(true)}
+				onTouchEnd={() => setExplorerHover(false)}>
+				<Stack direction='column' spacing={1} component='a' href='/explorer-hat' sx={{ textDecoration: 'none' }}>
+					<Card raised>
+						<Box sx={{ display: 'flex', height: '100%' }}>
+							<img src={explorerHover ? ExplorerThumbnailHover : ExplorerThumbnail} alt='Explorer Hat' className='shop-card-content ' />
+						</Box>
+					</Card>
+
+					<Card raised>
+						<Stack direction={'row'} justifyContent='space-between' p={1}>
+							<Typography>Explorer Hat</Typography>
+							<Typography>$32.50</Typography>
+						</Stack>
+					</Card>
+				</Stack>
 			</Grid>
 
-			<Grid item sm={12} md={6} className=' shop-card'>
-				<Card raised>
-					<Box sx={{ display: 'flex', height: '100%' }}>
-						<img
-							src={krownHover ? KrownThumbnailHover : KrownThumbnail}
-							onMouseOver={() => setKrownHover(true)}
-							onMouseOut={() => setKrownHover(false)}
-							onTouchStart={() => setKrownHover(true)}
-							onTouchEnd={() => setKrownHover(false)}
-							alt='Krown Hat'
-							className='shop-card-content'
-						/>
-					</Box>
-				</Card>
+			<Grid
+				item
+				sm={12}
+				md={4}
+				className=' shop-card-content'
+				onMouseOver={() => setKrownHover(true)}
+				onMouseOut={() => setKrownHover(false)}
+				onTouchStart={() => setKrownHover(true)}
+				onTouchEnd={() => setKrownHover(false)}>
+				<Stack direction='column' spacing={1}>
+					<Card raised>
+						<Box sx={{ display: 'flex', height: '100%' }}>
+							<img src={krownHover ? KrownThumbnailHover : KrownThumbnail} alt='Krown Hat' className='shop-card-content' />
+						</Box>
+					</Card>
+
+					<Card raised>
+						<Stack direction={'row'} justifyContent='space-between' p={1}>
+							<Typography>Krown Hat</Typography>
+							<Typography>TBD</Typography>
+						</Stack>
+					</Card>
+				</Stack>
 			</Grid>
 		</Grid>
 	);
